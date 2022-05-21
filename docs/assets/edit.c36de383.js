0ad606da1cf611fa5de8648b0c29cab3974f8f29
web-ljc <aaaa4l@qq.com>
2022-05-21 09:52:02 +0800

    feat: 微前端打包测试

--- docs/assets/edit.c36de383.js
+++
@@ -1,1 +0,0 @@
-import{D as t}from"./detail.0fb4b055.js";import{_ as e}from"./index.ec7e3197.js";import{c as r,r as s,o as a}from"./vendor.2cb241fc.js";import"./userModel.96b4559b.js";import"./request.df1cff29.js";const n={components:{Detail:t}};function c(p,i,m,_,d,f){const o=s("detail");return a(),r(o,{"is-edit":!0})}var v=e(n,[["render",c]]);export{v as default};
